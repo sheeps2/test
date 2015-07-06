43602cdbb5a27a80b432f498c5f3ef09bc8bc7ef
yura <dearjur@gmail.com>
2015-07-06 16:36:18 +0900

    소스 조금 변경

--- testC3.c
+++ testC3.c
@@ -2,21 +2,21 @@
 
 main(){
 	int i, j, n, count;
-	scanf("%d", &n);
-	
-	count=0;
-	for(i=0;i<n*2;i++){
-		for(j=0;j<n-count;j++)	printf(" ");
+	scanf_s("%d", &n);
 
-		if(i==0||i==(n*2)-1)	printf("*");
+	count = 0;
+	for (i = 0; i<=n * 2; i++){
+		for (j = 0; j<n - count; j++)	printf(" ");
+
+		if (i == 0 || i == (n * 2))	printf("*");
 		else{
 			printf("*");
-			for(j=0;j<count;j++)	printf(" ");
+			for (j = 0; j<count*2-1; j++)	printf(" ");
 			printf("*");
 		}
-
-		if(i<n) count++;
+		printf("\n");
+		if (i<n) count++;
 		else count--;
 	}
-
+	scanf_s("%d", &n);
 }
